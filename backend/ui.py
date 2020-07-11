d5725b4cf54dd6855275867a43db93fdab025c30
Aashish Ananthanarayan <aashish.2704@tamu.edu>
2020-07-11 10:59:12 -0500

    Initial uI

--- backend/ui.py
+++ backend/ui.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, session, request
 app = Flask(__name__)
+
+app.secret_key = "Flask123"
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('template1.html')
 
-@app.route('/help')
-def help():
+@app.route('/offerings')
+def offerings():
+
+    return render_template("offerings.html")
+
+@app.route('/post')
+def post():
+    return render_template("post.html")
+
+
+@app.route('/filters')
+def filters():  
     return 'help'
 
+@app.route('/community')
+def community():
+    return 'help'
 
 
 if __name__ == '__main__':
